8eeac7c6d0b5a44b159595936729dee8b86a1e66
pavan63031 <pavanreddy63031@gmail.com>
2025-11-01 13:22:02 +0530

    Moved API key to .env and updated Weather component

--- frontend/src/components/Weather.jsx
+++ frontend/src/components/Weather.jsx
@@ -8,7 +8,7 @@ function Weather() {
   useEffect(() => {
     const fetchWeather = async (lat, lon) => {
       try {
-        const API_KEY = "7a1494120bc9e98daf00f5255ecf1706";
+       const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
         const res = await axios.get(
           `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
         );
