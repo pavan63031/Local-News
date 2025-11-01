bef5849dbb2b852727428f42beccf0449fde19e1
pavan63031 <pavanreddy63031@gmail.com>
2025-11-01 19:23:12 +0530

    Added CORS configuration for frontend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://local-news-frontend.onrender.com"],
   credentials: true, 
 }));
 app.use('/auth',authRoutes);
